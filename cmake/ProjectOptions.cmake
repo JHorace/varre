b6b84957052c5de55906f68a4014455613f0de6d
jsumihiro <jasumihiro@gmail.com>
2026-04-11 11:28:08 -0700

    Initial CMake project structure conversion

---
+++ cmake/ProjectOptions.cmake
@@ -0,0 +1,10 @@
+include_guard(GLOBAL)
+
+set(CMAKE_CXX_STANDARD 23)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+option(VARRE_BUILD_ASSETS "Configure the varre-assets subtree." ON)
+option(VARRE_BUILD_ENGINE "Configure the varre-engine subtree." ON)
+option(VARRE_BUILD_APPS "Configure the varre-app subtree." ON)
+
